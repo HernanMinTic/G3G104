bdd23cdec40966552f21c22a4bc8540f317c0c81
Hernan Montoya <hernan-montoya@hotmail.com>
2022-11-29 13:14:06 -0500

    Project Sprint 1

--- app/src/main/java/com/example/shopg3g104/ProductDetails.java
+++ app/src/main/java/com/example/shopg3g104/ProductDetails.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProductDetails extends AppCompatActivity {
 
     private Button btnDetPro;
-    private TextView txtProName, txtDetPro;
+    private TextView txtProName, txtDetPro, txtDesPro, txtPriPro;
+    private ImageView imgDetPro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +22,19 @@ public class ProductDetails extends AppCompatActivity {
 
         btnDetPro = (Button) findViewById(R.id.btnDetPro);
         txtProName = (TextView) findViewById(R.id.txtProName);
-        txtDetPro = (TextView) findViewById(R.id.btnDetPro);
+        txtDetPro = (TextView) findViewById(R.id.txtDetPro);
+        txtDesPro = (TextView) findViewById(R.id.txtDesPro);
+        txtPriPro = (TextView) findViewById(R.id.txtPriPro);
+        imgDetPro = (ImageView) findViewById(R.id.imgDetPro);
 
         Intent intendIn = getIntent();
         txtProName.setText(intendIn.getStringExtra("name"));
+        txtPriPro.setText(intendIn.getStringExtra("price"));
+        txtDesPro.setText(intendIn.getStringExtra("description"));
+
+        int imageCodeBlank = intendIn.getIntExtra("imageCodeBlank", 0);
+        int codeImage = intendIn.getIntExtra("imageCode", imageCodeBlank);
+        imgDetPro.setImageResource(codeImage);
 
         btnDetPro.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/shopg3g104/Products.java
+++ app/src/main/java/com/example/shopg3g104/Products.java
@@ -4,17 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Products extends AppCompatActivity {
 
-    private Button btnPro1;
-    private Button btnPro2;
-    private Button btnPro3;
-
-    private TextView txtPro1, txtPro2, txtPro3;
+    private Button btnPro1, btnPro2, btnPro3;
+    private TextView txtPro1, txtPro2, txtPro3, txtPriPro1, txtPriPro2, txtPriPro3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +24,38 @@ public class Products extends AppCompatActivity {
         txtPro1 = (TextView) findViewById(R.id.txtPro1);
         txtPro2 = (TextView) findViewById(R.id.txtPro2);
         txtPro3 = (TextView) findViewById(R.id.txtPro3);
+        txtPriPro1 = (TextView) findViewById(R.id.txtPriPro1);
+        txtPriPro2 = (TextView) findViewById(R.id.txtPriPro2);
+        txtPriPro3 = (TextView) findViewById(R.id.txtPriPro3);
 
-        btnPro1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ProductDetails.class);
-                intent.putExtra("name", txtPro1.getText().toString());
-                startActivity(intent);
-            }
+        btnPro1.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), ProductDetails.class);
+            intent.putExtra("name", txtPro1.getText().toString());
+            intent.putExtra("price", txtPriPro1.getText().toString());
+            intent.putExtra("description", "Excellent");
+            intent.putExtra("imageCode", R.drawable.product1);
+            intent.putExtra("imageCodeBlank", R.drawable.blank_image);
+            startActivity(intent);
         });
 
-        btnPro2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ProductDetails.class);
-                intent.putExtra("name", txtPro2.getText().toString());
-                startActivity(intent);
-            }
+        btnPro2.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), ProductDetails.class);
+            intent.putExtra("name", txtPro2.getText().toString());
+            intent.putExtra("price", txtPriPro2.getText().toString());
+            intent.putExtra("description", "Great");
+            intent.putExtra("imageCode", R.drawable.product2);
+            intent.putExtra("imageCodeBlank", R.drawable.blank_image);
+            startActivity(intent);
         });
 
-        btnPro3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ProductDetails.class);
-                intent.putExtra("name", txtPro3.getText().toString());
-                startActivity(intent);
-            }
+        btnPro3.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), ProductDetails.class);
+            intent.putExtra("name", txtPro3.getText().toString());
+            intent.putExtra("price", txtPriPro3.getText().toString());
+            intent.putExtra("description", "Good");
+            intent.putExtra("imageCode", R.drawable.product3);
+            intent.putExtra("imageCodeBlank", R.drawable.blank_image);
+            startActivity(intent);
         });
     }
 }
