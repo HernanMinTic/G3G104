ab9b68047424c327a60ec7e0c837bf5e5f24935a
Hernan Montoya <hernan-montoya@hotmail.com>
2022-12-20 02:37:12 -0500

    Login

--- app/src/main/java/com/example/shopg3g104/Home.java
+++ app/src/main/java/com/example/shopg3g104/Home.java
@@ -21,7 +21,7 @@ public class Home extends AppCompatActivity {
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Products.class);
+                Intent intent = new Intent(getApplicationContext(), Login.class);
                 startActivity(intent);
             }
         });
